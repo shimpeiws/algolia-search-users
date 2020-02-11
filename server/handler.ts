89ef92937d2487e8aed035bd04f5735253764466
Shimpei Takamatsu <shimpeiws@gmail.com>
2020-02-12 02:11:22 +0900

    Enable CORS

--- server/handler.ts
+++ server/handler.ts
@@ -147,6 +147,10 @@ export const securedApiKey: APIGatewayProxyHandler = async (
   );
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Credentials": true
+    },
     body: JSON.stringify({ key: publicKey })
   };
 };
